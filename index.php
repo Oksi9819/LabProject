cc623bb60bfef8af076b9a87d6853a7634edb5af
OksiFoxy <oksanaostapuk@gmail.com>
2022-09-07 18:02:24 +0300

    Added steampixel/simple-php-router.

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-echo "Hello World!";
-$enddds=32;
-echo $enddds;
+// Autoload files using composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Use this namespace
+use Steampixel\Route;
+
+// Add your first route
+Route::add('/', function() {
+  echo 'Welcome :-)';
+});
+
+// Run the router
+Route::run('/');
 ?>
