851aba52abdbfe8ae6776eb6072bace05bfe1c4b
OksiFoxy <oksanaostapuk@gmail.com>
2022-09-12 12:04:27 +0300

    Edit keys of UserInfo.

--- src/View/UserView.php
+++ src/View/UserView.php
@@ -7,12 +7,12 @@ class UserView
     public function renderUserPage($user)
     {
         echo "Данные о пользователе: <br>";
-        echo "Id: ".$user[user_id]."<br>";
-        echo "Фамилия: ".$user[user_name]."<br>";
-        echo "Имя: ".$user[user_surname]."<br>";
-        echo "День рождения: ".$user[user_birthday]."<br>";
-        echo "Номер телефона: ".$user[user_phone]."<br>";
-        echo "Адрес: ".$user[user_address]."<br>";
-        echo "Email: ".$user[user_email]."<br>";
+        echo "Id: ".$user['user_id']."<br>";
+        echo "Фамилия: ".$user['user_name']."<br>";
+        echo "Имя: ".$user['user_surname']."<br>";
+        echo "День рождения: ".$user['user_birthday']."<br>";
+        echo "Номер телефона: ".$user['user_phone']."<br>";
+        echo "Адрес: ".$user['user_address']."<br>";
+        echo "Email: ".$user['user_email']."<br>";
     }
 }
