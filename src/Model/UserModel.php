97909af552d08ee1fd03593dd4a2c1ab7a436509
OksiFoxy <oksanaostapuk@gmail.com>
2022-09-15 19:54:56 +0300

    Added Read functions to UserModel.

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -21,7 +21,7 @@ class UserModel
         $user_password = hash('md5', (string)$_POST['user_password']);
         if (isset($_POST['submit'])) {
             if (strlen($_POST['user_password']) < 9) {
-                $sql = ("INSERT INTO `user`(`user_surname`, `user_name`, `user_birthday`, `user_phone`, `user_address`, `user_email`, `user_password`) VALUES(?,?,?,?,?,?,?)");
+                $sql = "INSERT INTO `user`(`user_surname`, `user_name`, `user_birthday`, `user_phone`, `user_address`, `user_email`, `user_password`) VALUES(?,?,?,?,?,?,?)";
                 $query = $conn->prepare($sql);
                 $query->bind_param('sssssss', $user_surname, $user_name, $user_birthday, $user_phone, $user_address, $user_email, $user_password);
 	            $query->execute();
@@ -31,6 +31,27 @@ class UserModel
         } 
     }
 
+    //READ
+    public function getUsers(): array
+    {
+        global $conn;
+        $sql = "SELECT * FROM `user`";
+        $query = $conn->prepare($sql);
+        $query->execute();
+        $result = $query->fetchAll();
+    }
+
+    public function getUserInfo(int $user_id): array
+    {
+        global $conn;
+        $sql = "SELECT * FROM `user` WHERE `user_id` = ?";
+        $query = $conn->prepare($sql);
+        $query->bind_param('i', $user_id);
+        $query->execute();
+        $result = $query->fetchAll(); 
+    }
+
+    //UPDATE
     /*public function authUser():array
     {
         
@@ -38,22 +59,10 @@ class UserModel
         return $user;
     }*/
 
+    //DELETE
     /*public function deleteUser(array $user):void
     {
         unset($user);
     }*/
 
-    public function getUserInfo(int $user_id): array
-    {
-            $user = array(
-            "user_id"=>$user_id,
-            "user_name"=>"Смирнов",
-            "user_surname"=>"Петр",
-            "user_birthday"=>"01/01/1998",
-            "user_phone"=>"+375 (29) 111-11-11",
-            "user_address"=>"г. Минск, пр-т Независимости, 116",
-            "user_email"=>"testclient@gmail.com",
-            );
-            return $user;
-    }
 }
