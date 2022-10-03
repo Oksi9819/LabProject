71f08357ad336630b33a39eb78c73ad574ef2b94
OksiFoxy <oksanaostapuk@gmail.com>
2022-10-03 10:41:59 +0300

    Edited admin permissions using sessions.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Itechart\InternshipProject\Controller;
 
+session_start();
+
 use Itechart\InternshipProject\Controller\BasicController;
 use Itechart\InternshipProject\View\ProductView;
 use Itechart\InternshipProject\Model\ProductModel;
@@ -85,88 +87,138 @@ class ProductController extends BasicController
     //Admin functions
     public function addProduct()
     {
-        global $BASEPATH;
-        $values = array();
-        if (!empty($_POST['submit_update_product'])) {
-            if (!empty($_POST['prod_name']) && !empty($_POST['prod_desc']) && !empty($_POST['prod_price']) && !empty($_POST['id_new_prod_category'])) {
-                $product_name = (string)$_POST['prod_name'];
-                $product_desc = (string)$_POST['prod_desc'];
-                $product_category = (int)$_POST['id_new_prod_category'];
-                $product_price = (float)$_POST['prod_price'];
-                $product=$this->productModel->setProduct($product_name, $product_desc, $product_category, $product_price);
-                return header('Location: '.BASEPATH.'catalog');
-            }  
+        if(!$_SESSION['user']) {
+            echo "You have no permissions to do this action. Available only for administrators.";
+		} else {
+            if($_SESSION['user']['role'] === "Admin") {
+                global $BASEPATH;
+                $values = array();
+                if (!empty($_POST['submit_update_product'])) {
+                    if (!empty($_POST['prod_name']) && !empty($_POST['prod_desc']) && !empty($_POST['prod_price']) && !empty($_POST['id_new_prod_category'])) {
+                        $product_name = (string)$_POST['prod_name'];
+                        $product_desc = (string)$_POST['prod_desc'];
+                        $product_category = (int)$_POST['id_new_prod_category'];
+                        $product_price = (float)$_POST['prod_price'];
+                        $product=$this->productModel->setProduct($product_name, $product_desc, $product_category, $product_price);
+                        return header('Location: '.BASEPATH.'catalog');
+                    }  
+                }               
+            } else {
+                echo "You have no permissions to do this action. Available only for administrators.";   
+            }
         }
     }
 
     public function updateProduct(int $product_id)
     {
-        global $BASEPATH;
-        $product_id = (int)$product_id;
-        $field = array();
-        $value = array();
-        $types = "";
-        if (!empty($_POST['submit_update_product'])) {
-            if (!empty($_POST['product_name'])) {
-                $product_name = (string)$_POST['product_name'];
-                array_push($field, "product_name");
-                array_push($value, $product_name);
-                $types.="s";
+        if(!$_SESSION['user']) {
+            echo "You have no permissions to do this action. Available only for administrators.";
+		} else {
+            if($_SESSION['user']['role'] === "Admin") {
+                global $BASEPATH;
+                $product_id = (int)$product_id;
+                $field = array();
+                $value = array();
+                $types = "";
+                if (!empty($_POST['submit_update_product'])) {
+                    if (!empty($_POST['product_name'])) {
+                        $product_name = (string)$_POST['product_name'];
+                        array_push($field, "product_name");
+                        array_push($value, $product_name);
+                        $types.="s";
+                    }
+                    if (!empty($_POST['product_desc'])) {
+                        $product_desc = (string)$_POST['product_desc'];
+                        array_push($field, "product_desc");
+                        array_push($value, $product_desc);
+                        $types.="s";
+                    }
+                    if (!empty($_POST['product_price'])) {
+                        $product_price = (float)$_POST['product_price'];
+                        array_push($field, "product_price");
+                        array_push($value, $product_price);
+                        $types.="d";
+                    }
+                    if (!empty($value)) {
+                        $product=$this->productModel->updateProduct($field, $product_id, $value, $types);
+                        return header('Location: '.BASEPATH.'catalog/id'.$product_id);
+                    }
+                }           
+            } else {
+                echo "You have no permissions to do this action. Available only for administrators.";   
             }
-            if (!empty($_POST['product_desc'])) {
-                $product_desc = (string)$_POST['product_desc'];
-                array_push($field, "product_desc");
-                array_push($value, $product_desc);
-                $types.="s";
-            }
-            if (!empty($_POST['product_price'])) {
-                $product_price = (float)$_POST['product_price'];
-                array_push($field, "product_price");
-                array_push($value, $product_price);
-                $types.="d";
-            }
-            if (!empty($value)) {
-                $product=$this->productModel->updateProduct($field, $product_id, $value, $types);
-                return header('Location: '.BASEPATH.'catalog/id'.$product_id);
-            } 
         }
     }
 
     public function deleteProduct(int $product_id)
     {
-        if (!empty($_POST['submit_delete_product'])) {
-            $result=$this->productModel->deleteProduct($product_id);
-            return $this->productView->renderProductDeletedPage($result, $product_id);
+        if(!$_SESSION['user']) {
+            echo "You have no permissions to do this action. Available only for administrators.";
+		} else {
+            if($_SESSION['user']['role'] === "Admin") {
+                if (!empty($_POST['submit_delete_product'])) {
+                    $result=$this->productModel->deleteProduct($product_id);
+                    return $this->productView->renderProductDeletedPage($result, $product_id);
+                }           
+            } else {
+                echo "You have no permissions to do this action. Available only for administrators.";   
+            }
         }
     }
 
-    public function addProductCategory() {
-        global $BASEPATH;
-        if (!empty($_POST['submit_add_category'])) {
-                $new_category = trim((string)$_POST['category_name']);
-                $new_category_eng = trim((string)$_POST['category_eng']);
-                $result = (new CategoryModel())->setCategory($new_category, $new_category_eng);
-                return header('Location: '.BASEPATH.'catalog');
+    public function addProductCategory() 
+    {
+        if(!$_SESSION['user']) {
+            echo "You have no permissions to do this action. Available only for administrators.";
+		} else {
+            if($_SESSION['user']['role'] === "Admin") {
+                global $BASEPATH;
+                if (!empty($_POST['submit_add_category'])) {
+                        $new_category = trim((string)$_POST['category_name']);
+                        $new_category_eng = trim((string)$_POST['category_eng']);
+                        $result = (new CategoryModel())->setCategory($new_category, $new_category_eng);
+                        return header('Location: '.BASEPATH.'catalog');
+                }           
+            } else {
+                echo "You have no permissions to do this action. Available only for administrators.";   
+            }
         }
     }
 
-    public function updateProductCategory() {
-        global $BASEPATH;
-        if (!empty($_POST['submit_update_category'])) {
-            $category_id = $_POST['update_id_category'];
-            $new_category = trim((string)$_POST['new_category']);
-            $new_category_eng = trim((string)$_POST['new_category_eng']);
-            $result = (new CategoryModel())->updateCategory($category_id, $new_category, $new_category_eng);
-            return header('Location: '.BASEPATH.'catalog');
-        }   
+    public function updateProductCategory() 
+    {
+        if(!$_SESSION['user']) {
+            echo "You have no permissions to do this action. Available only for administrators.";
+		} else {
+            if($_SESSION['user']['role'] === "Admin") {
+                global $BASEPATH;
+                if (!empty($_POST['submit_update_category'])) {
+                    $category_id = $_POST['update_id_category'];
+                    $new_category = trim((string)$_POST['new_category']);
+                    $new_category_eng = trim((string)$_POST['new_category_eng']);
+                    $result = (new CategoryModel())->updateCategory($category_id, $new_category, $new_category_eng);
+                    return header('Location: '.BASEPATH.'catalog');
+                }              
+            } else {
+                echo "You have no permissions to do this action. Available only for administrators.";   
+            }
+        }  
     }
 
     public function deleteProductCategory()
     {
-        if (!empty($_POST['submit_delete_category'])) {
-            $category_id = $_POST['id_del_category'];
-            $result = (new CategoryModel())->deleteCategory($category_id);
-            return header('Location: '.BASEPATH.'catalog');
-        } 
+        if(!$_SESSION['user']) {
+            echo "You have no permissions to do this action. Available only for administrators.";
+		} else {
+            if($_SESSION['user']['role'] === "Admin") {
+                if (!empty($_POST['submit_delete_category'])) {
+                    $category_id = $_POST['id_del_category'];
+                    $result = (new CategoryModel())->deleteCategory($category_id);
+                    return header('Location: '.BASEPATH.'catalog');
+                }               
+            } else {
+                echo "You have no permissions to do this action. Available only for administrators.";   
+            }
+        }  
     } 
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace Itechart\InternshipProject\Controller;
+
+session_start();
 
 use Exception;
 use Itechart\InternshipProject\View\UserView;
@@ -89,14 +90,16 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             if ($_SESSION['user']['role'] === "Admin") {
-                $user = $this->userModel->getUserInfo($user_id);
+                $user = $this->userModel->getUserInfo($_SESSION['user']['id']);
                 return $this->userView->renderAdminPage($user);
             } else {
-                $user = $this->userModel->getUserInfo($user_id);
+                $user = $this->userModel->getUserInfo($_SESSION['user']['id']);
                 return $this->userView->renderUserPage($user);
             }
+        } else {
+            header("Location: /");
         }
     }
 
@@ -104,15 +107,17 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             if ($_SESSION['user']['role'] === "Admin") {
                 echo "YOU ARE ADMIN";
                 $reviews = (new ReviewModel())->getReviews();
                 return $this->userView->renderAdminReviewsPage($reviews);
             } else {
-                $reviews = (new ReviewModel())->getReviewsByUserId($user_id);
-                return $this->userView->renderUserReviewsPage($reviews, $user_id);
+                $reviews = (new ReviewModel())->getReviewsByUserId($_SESSION['user']['id']);
+                return $this->userView->renderUserReviewsPage($reviews, $_SESSION['user']['id']);
             }    
+        } else {
+            header("Location: /");
         }
     }
 
@@ -120,14 +125,16 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             global $BASEPATH;
             if (!empty($_POST['submit_set_review'])) {
                 $reviewText = (string)$_POST['reviewText'];
-                echo $reviewText."<br>".$user_id;
-                $new_review = (new ReviewModel())->setReview($user_id, $reviewText);
-                return header('Location: '.BASEPATH.'profile/'.$user_id.'/reviews');
+                echo $reviewText."<br>".$_SESSION['user']['id'];
+                $new_review = (new ReviewModel())->setReview($_SESSION['user']['id'], $reviewText);
+                return header('Location: '.BASEPATH.'profile/'.$_SESSION['user']['id'].'/reviews');
             }
+        } else {
+            header("Location: /");
         }
     }
 
@@ -135,14 +142,16 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             global $BASEPATH;
             if (!empty($_POST['submit_update_review_text'])) {
                 $review_id = (int)$_POST['id_review'];
                 $review_text = (string)$_POST['newReviewText'];
                 $updated_review = (new ReviewModel())->updateReview($review_id, $review_text);
-                return header('Location: '.BASEPATH.'profile/'.$user_id.'/reviews');
+                return header('Location: '.BASEPATH.'profile/'.$_SESSION['user']['id'].'/reviews');
             }
+        } else {
+            header("Location: /");
         }
     }
 
@@ -150,13 +159,15 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             global $BASEPATH;
             if (!empty($_POST['submit_delete_review'])) {
                 $review_id = (int)$_POST['id_review_delete'];
                 $result = (new ReviewModel())->deleteReview($review_id);
-                return header('Location: '.BASEPATH.'profile/'.$user_id.'/reviews');
+                return header('Location: '.BASEPATH.'profile/'.$_SESSION['user']['id'].'/reviews');
             }
+        } else {
+            header("Location: /");
         }
     }
 
@@ -164,7 +175,7 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             if ($_SESSION['user']['role'] === "Admin") {
                 echo "YOU ARE ADMIN";
                 $orders = (new OrderModel())->getOrders();
@@ -172,10 +183,12 @@ class UserController extends BasicController
                 return $this->userView->renderAdminOrdersPage($orders, $order_details);
             } else {
                 //echo "YOU ARE NOT ADMIN";
-                $orders = (new OrderModel())->getOrdersByUserId($user_id);
-                $order_details = (new OrderModel())->getOrderDetails($user_id);
-                return $this->userView->renderUserOrdersPage($orders, $order_details, $user_id);
+                $orders = (new OrderModel())->getOrdersByUserId($_SESSION['user']['id']);
+                $order_details = (new OrderModel())->getOrderDetails($_SESSION['user']['id']);
+                return $this->userView->renderUserOrdersPage($orders, $order_details, $_SESSION['user']['id']);
             }
+        } else {
+            header("Location: /");
         }
     }
 
@@ -183,22 +196,26 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} elseif ($_SESSION['user']['role'] === "Admin") {
-            if (!empty($_POST['submit_reg_admin'])) {
-                if (strlen($_POST['admin_password']) < 9) {
-                    $user_surname = (string)$_POST['admin_surname'];
-                    $user_name = (string)$_POST['admin_name'];
-                    $user_birthday = (string)$_POST['admin_birthday'];
-                    $user_phone = (string)$_POST['admin_phone'];
-                    $user_address = (string)$_POST['admin_address'];
-                    $user_email = (string)$_POST['admin_email'];
-                    $user_password = hash('md5', (string)$_POST['user_password']);
-                    $values = array ($user_surname, $user_name, $user_birthday, $user_phone, $user_address, $user_email, $user_password, "2");
-                    $result = $this->userModel->setAdmin($values);
-                    return header('Location: '.BASEPATH.'profile/'.$user_id);
-                } else {
-                    echo "Password length should be not less than 9 characters.";
-                }   
+		} elseif ($_SESSION['user']['id'] == $user_id) {
+            if ($_SESSION['user']['role'] === "Admin") {
+                if (!empty($_POST['submit_reg_admin'])) {
+                    if (strlen($_POST['admin_password']) < 9) {
+                        $user_surname = (string)$_POST['admin_surname'];
+                        $user_name = (string)$_POST['admin_name'];
+                        $user_birthday = (string)$_POST['admin_birthday'];
+                        $user_phone = (string)$_POST['admin_phone'];
+                        $user_address = (string)$_POST['admin_address'];
+                        $user_email = (string)$_POST['admin_email'];
+                        $user_password = hash('md5', (string)$_POST['user_password']);
+                        $values = array ($user_surname, $user_name, $user_birthday, $user_phone, $user_address, $user_email, $user_password, "2");
+                        $result = $this->userModel->setAdmin($values);
+                        return header('Location: '.BASEPATH.'profile/'.$_SESSION['user']['id']);
+                    } else {
+                        echo "Password length should be not less than 9 characters.";
+                    }   
+                }
+            } else {
+                header("Location: /");
             }
         } else {
             header("Location: /");
@@ -209,9 +226,8 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             global $BASEPATH;
-            $user_id = (int)$user_id;
             $field = array();
             $value = array();
             $types = "";
@@ -253,10 +269,12 @@ class UserController extends BasicController
                     $types.="s";
                 }
                 if (!empty($value)) {
-                    $user = $this->userModel->updateUser($field, $user_id, $value, $types);
-                    return header('Location: '.BASEPATH.'profile/'.$user_id);
+                    $user = $this->userModel->updateUser($field, $_SESSION['user']['id'], $value, $types);
+                    return header('Location: '.BASEPATH.'profile/'.$_SESSION['user']['id']);
                 }     
             }
+        } else {
+            header("Location: /");
         }   
     }
         
@@ -264,9 +282,8 @@ class UserController extends BasicController
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             global $BASEPATH;
-            $user_id = (int)$user_id;
             $field = array();
             $value = array();
             $types = "";
@@ -279,25 +296,30 @@ class UserController extends BasicController
                     array_push($value, $user_password);
                     $types.="s";
                     if (!empty($value)) {
-                        $user = $this->userModel->updateUser($field, $user_id, $value, $types);
-                        return header('Location: '.BASEPATH.'profile/'.$user_id);
+                        $user = $this->userModel->updateUser($field, $_SESSION['user']['id'], $value, $types);
+                        return header('Location: '.BASEPATH.'profile/'.$_SESSION['user']['id']);
                     }  
                 } else {
                     echo "Passwords don't match";
                 }
             }
-        }
+        }  else {
+            header("Location: /");
+        } 
     }
     
     public function deleteUser(int $user_id)
     {
         if(!$_SESSION['user']) {
 			header("Location: /");
-		} else {
+		} elseif ($_SESSION['user']['id'] == $user_id) {
             if (!empty($_POST['submit_delete_user'])) {
-                $result=$this->userModel->deleteUser($user_id);
+                $result=$this->userModel->deleteUser($_SESSION['user']['id']);
+                session_destroy();
                 return $this->userView->renderUserDeletedPage($result, $user_id);
             }
-        }
+        } else {
+            header("Location: /");
+        } 
     }
 }
