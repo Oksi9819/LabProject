8348cc08c9340cb7a5abd117f3723ee9e28049f0
OksiFoxy <oksanaostapuk@gmail.com>
2022-10-18 00:46:32 +0300

    Compiled webpack

--- front_src/webpack.config.js
+++ front_src/webpack.config.js
@@ -1,8 +1,9 @@
+const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
        filename: 'bundle.js',
-       path: path.resolve(__dirname, 'dist')
+       path: path.resolve(__dirname, '/dist')
     },
     module: {
         rules: [
@@ -22,5 +23,6 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    mode: 'development'
 };
