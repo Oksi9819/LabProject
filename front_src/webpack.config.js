34b31419eaa5b8f93f107559ae788f616b01f731
OksiFoxy <oksanaostapuk@gmail.com>
2022-10-17 17:58:32 +0300

    Added less, style-loader, less-loader, css-loader; Added webpack.config.js.

---
+++ front_src/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+       filename: 'bundle.js',
+       path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test:/\.less$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "less-loader" }
+                ]
+            }
+        ]
+    }
+};
