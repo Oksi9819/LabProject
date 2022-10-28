6fbecda2b8e63907bd068eac1b5e9d6cdacb23ca
OksiFoxy <oksanaostapuk@gmail.com>
2022-10-28 11:20:22 +0300

    Added jQuery.

--- front_src/src/index.js
+++ front_src/src/index.js
@@ -3,5 +3,4 @@ import '/src/styles.less';
 <script 
     src="https://code.jquery.com/jquery-3.6.1.js" 
     integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" 
-    crossorigin="anonymous"
-></script>
+    crossorigin="anonymous"></script>
